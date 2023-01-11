f01bb490ec8aba55eac8ffdf9c7a6147aaac51cc
CodesOfLS <122414460+CodesOfLS@users.noreply.github.com>
2023-01-10 22:09:13 -0300

    Informe seu nome.

---
+++ nome.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string nome;
+	
+	cout << "------------------------------\n";
+	cout << "      Bem-vind(a/o)!\n";
+	cout << "------------------------------\n";
+	cout << "Digite o seu nome: ";
+	cin >> nome;
+	
+	cout << nome << ", e um nome bonito.\n";
+	cout << "--------------------------------\n";
+	
+	system("pause");
+	return 0;
+}
