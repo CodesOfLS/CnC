1a2498e6c97e7795d3574a0b9c59222d9a33c588
Human Code <122414460+CodesOfLS@users.noreply.github.com>
2023-01-12 01:14:30 -0300

    Somando dois números em C++

---
+++ CPP_Codes/soma.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+
+    cout << "Informe o primeiro valor: ";
+    cin >> n1;
+    cout << "Informe o segundo valor: ";
+    cin >> n2;
+
+    cout << "\nA soma de " << n1 << " e " << n2 << " e igual a " << n1 + n2 << endl;
+
+    system("pause");
+    return 0;
+}
